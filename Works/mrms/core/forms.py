c953b69070a0d7b480bfd5967d254926bb844416
Gautham Krishna P <gauthamkrishna1312@gmail.com>
2024-05-03 01:43:59 +0530

    appointment

--- Works/mrms/core/forms.py
+++ Works/mrms/core/forms.py
@@ -1,1 +1,32 @@
 from django import forms
+from .models import Hospital
+from users.models import Doctor
+
+class HospitalForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["beds"].widget.attrs["placeholder"] = "Beds"
+        self.fields["rooms"].widget.attrs["placeholder"] = "Rooms"
+        self.fields["description"].widget.attrs["placeholder"] = "Description"
+
+    class Meta:
+        model = Hospital
+        fields = ['beds', 'rooms', 'description']
+
+class DoctorForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["qualification"].widget.attrs["placeholder"] = "Qualification"
+        self.fields["experties"].widget.attrs["placeholder"] = "Experties"
+        self.fields["experience"].widget.attrs["placeholder"] = "Experience"
+        self.fields["bio"].widget.attrs["placeholder"] = "Bio"
+
+    class Meta:
+        model = Doctor
+        fields = ['qualification', 'experties', 'experience', 'bio']
+
+
+class adddateForm(forms.Form):
+    time = forms.DateTimeField(widget=forms.DateTimeInput())
--- Works/mrms/core/views.py
+++ Works/mrms/core/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 from django.views import View, generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import get_user_model
 
-from . import models
+from .forms import HospitalForm,DoctorForm, adddateForm
+from . import models, mixins
+from users.django_mail.views import SendEmailView, generate_uidb64_url
 
 
 class IndexView(generic.TemplateView):
@@ -22,3 +26,148 @@ class AppointmentView(LoginRequiredMixin, View):
     def post(self, request, *args, **kwargs):
         doctor = request.POST.get('doctor')
         appointment = models.Appointment(doctor=doctor, customer=request.user)
+        appointment.save()
+        request.session["DOCTOR_EMAIL"] = doctor.user.email
+        return redirect(reverse_lazy("core:appointment-confirmation-mail"))
+    
+
+
+class AppointmentSendEmail(SendEmailView):
+    email_subject = "Appointment Request"
+    send_html_email = True
+    email_template_name = "appointment-confirmation-mail.html"
+
+    def get_from_email(self):
+        return self.request.user.get_email_field_name
+    
+    def get_to_email(self):
+        return self.request.session.pop("DOCTOR_EMAIL")
+    
+    def get_email_context_data(self):
+        return {
+            "patient": self.request.user,
+        }
+    
+    def get_success_url(self):
+        return reverse_lazy("users:profile-customer", kwargs={"username": self.request.user.username})
+
+
+class AppointmentDecline(mixins.RoleRequiredMixin, View):
+    role = get_user_model().DOCTOR
+
+    def get(self, request, **kwargs):
+        appointment = get_object_or_404(models.Appointment, id=kwargs.get("id"))
+        appointment.status = models.Appointment.DECLINED
+        appointment.save()
+        self.request.session["APPO_ID"] = appointment.id
+        return redirect(reverse_lazy("users:profile-doctor", kwargs={"username": self.request.user.username}))
+    
+
+class AppointmentAccept(mixins.RoleRequiredMixin, View):
+    role = get_user_model().DOCTOR
+    form_class = adddateForm
+
+    def get_appointment_model(self):
+        return get_object_or_404(models.Appointment, id=self.kwargs.get("id"))
+    
+    def render_to_response(self, context):
+        return render(self.request, "confirm-appointment.html", context)
+    
+    def get_context_data(self, **kwargs):
+        return {"appointment": self.get_appointment_model(), **kwargs}
+
+    def get(self, request, **kwargs):
+        return self.render_to_response(self.get_context_data(form=self.form_class))
+    
+    def form_valid(self, form):
+        time = form.cleaned_data["time"]
+        appointment = self.get_appointment_model()
+        appointment.time = time
+        appointment.save()
+        self.request.session["PATIENT_EMAILID"] = appointment.customer.user.email
+        self.request.session["APPO_ID"] = appointment.id
+        return redirect(reverse_lazy("users:profile-doctor", kwargs={"username": self.request.user.username}))
+
+    def form_invalid(self, form):
+        return self.render_to_response(self.get_context_data(form=form))
+    
+    def post(self, request, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            self.form_valid(form)
+        else:
+            self.form_invalid(form)
+
+
+class AppointmentAcceptmail(SendEmailView):
+    email_subject = "Appointment Accepted"
+    send_html_email = True
+    email_template_name = "appointment-accept-mail.html"
+
+    def get_from_email(self):
+        return self.request.user.email
+    
+    def get_to_email(self):
+        return self.request.session.pop("PATIENT_EMAILID")
+    
+    def get_email_context_data(self):
+        return {
+            "appointment": get_object_or_404(models.Appointment, id=self.request.session.pop("APPO_ID")),
+            "status": "Accepted",
+        }
+
+
+
+class AppointmentDeclinemail(SendEmailView):
+    email_subject = "Appointment Declined"
+    send_html_email = True
+    email_template_name = "appointment-decline-mail.html"
+
+    def get_from_email(self):
+        return self.request.user.email
+    
+    def get_to_email(self):
+        return self.request.session.pop("PATIENT_EMAILID")
+    
+    def get_email_context_data(self):
+        return {
+            "appointment": get_object_or_404(models.Appointment, id=self.request.session.pop("APPO_ID")),
+            "status": "Declined",
+        }
+
+
+
+class HospitalDetails(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+            form = HospitalForm()  # Assuming you have a HospitalForm defined
+            return render(request, 'add_hospital_details.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = HospitalForm(request.POST)
+        if form.is_valid():
+            hospital = form.save(commit=False)
+            hospital.user = request.user
+            hospital.save()
+            return redirect('core:dashboard')  # Redirect to a dashboard or any other page after successful addition
+        else:
+            # If form is not valid, re-render the form with errors
+            return render(request, 'add_hospital_details.html', {'form': form})
+
+
+class DoctorDetails(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        form = DoctorForm()  # Assuming you have a HospitalForm defined
+        return render(request, 'add_doctor_details.html', {'form': form})
+    
+    def post(self, request, *args, **kwargs):
+        form = DoctorForm(request.POST)
+        if form.is_valid():
+            doctor = form.save(commit=False)
+            doctor.user = request.user
+            doctor.save()
+            return redirect('core:dashboard')  # Redirect to a dashboard or any other page after successful addition
+        else:
+            # If form is not valid, re-render the form with errors
+            return render(request, 'add_doctor_details.html', {'form': form})   
+    
+
