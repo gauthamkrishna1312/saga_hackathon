2da93bf72f55b141778d2d329e4210049d014812
Gautham Krishna P <gauthamkrishna1312@gmail.com>
2024-05-02 17:21:44 +0530

    profiles

--- Works/mrms/mrms/urls.py
+++ Works/mrms/mrms/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include(('users.urls', 'users'), namespace='users')),
-    path('/', include(('core.urls', 'users'), namespace='core'))
+    path('', include(('core.urls', 'users'), namespace='core'))
 ]
